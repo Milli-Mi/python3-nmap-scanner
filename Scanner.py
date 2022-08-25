559d2e7e81e300201bdaaa0c8e04218129af776d
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-25 05:09:38 +0300

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -11,3 +11,10 @@ print("<----------------------------------------------------->")
 ip_addr = input("Please enter the IP address you want to scan: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
+
+resp = input("""\n Please enter type of scan you want to run
+                 1)SYN ASK Scan
+                 2)UDP Scan
+                 3)Comprehensive Scan""")
+
+ print("You have selected the option: ", resp)                
