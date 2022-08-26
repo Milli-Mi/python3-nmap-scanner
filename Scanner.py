40044bd556166f53a1a63e9709d45a6e3849e5b6
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-26 09:23:51 +0300

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -12,9 +12,21 @@ ip_addr = input("Please enter the IP address you want to scan: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
 
-resp = input("""\n Please enter type of scan you want to run
+resp = input("""\nPlease enter type of scan you want to run
                  1)SYN ASK Scan
                  2)UDP Scan
-                 3)Comprehensive Scan""")
+                 3)Comprehensive Scan \n""")
 
- print("You have selected the option: ", resp)                
+print("You have selected the option: ", resp)
+
+if resp == '1':
+   print("Nmap Version: ", scanner.nmap_version())
+   scanner.scan(ip_addr, '1-1024', '-v -sS')
+   print(scanner.scaninfo())
+   print("IP Status: ", scanner[ip_addr].state())
+   print(scanner[ip_addr].all_protocols())
+   print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+  
+
+   
+ 
