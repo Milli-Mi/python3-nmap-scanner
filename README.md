715ce6001e4b1a93d274dfda2bf3abf4b6d32dcd
Milli-Mi <87952989+Milli-Mi@users.noreply.github.com>
2022-08-26 10:49:55 +0300

    update scanner.py complete

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 *What is the correct command to install the Python 3 version of the python-nmap library?*
 
 -pip3 install python-nmap
+
+*Which of the following allows you to scan for UDP ports between 21 to 443?*
+
+-.scan(ip_addr, '21-443', '-v -sU')
  
--- Scanner.py
+++ Scanner.py
@@ -26,7 +26,25 @@ if resp == '1':
    print("IP Status: ", scanner[ip_addr].state())
    print(scanner[ip_addr].all_protocols())
    print("Open ports: ", scanner[ip_addr]['tcp'].keys())
-  
+elif resp == '2':
+   print("Nmap Version: ", scanner.nmap_version())
+   scanner.scan(ip_addr, '1-1024', '-v -sU')
+   print(scanner.scaninfo())
+   print("IP Status: ", scanner[ip_addr].state())
+   print(scanner[ip_addr].all_protocols())
+   print("Open ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+   print("Nmap Version: ", scanner.nmap_version())
+   scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+   print(scanner.scaninfo())
+   print("IP Status: ", scanner[ip_addr].state())
+   print(scanner[ip_addr].all_protocols())
+   print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp >= "4":
+   print("Please enter a valid option")
+
+
+
 
    
  
